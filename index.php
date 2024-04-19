c292c29ed178c11c55ae0b1491515a69693c58a0
danskerid <danskerid@gmail.com>
2024-04-19 12:48:22 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<title>Portofolio DanskerID</title>
+	
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:image" content="assets/images/share.png">
+	<meta name="description" content="Portofolio M Hudan Lilmuttaqien">
+	<meta name="author" content="DanskerSweetID">    
+	<link rel="shortcut icon" href="assets/images/LOGODANSKERID.png"> 
+	
+	<!-- Google Fonts -->	
+	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	
+	<!-- Bootstrpa Icons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	
+	<!-- FontAwesome JS -->
+    <script defer src="assets/fontawesome/js/all.js"></script>
+
+	<!-- Theme CSS -->  
+	<link id="theme-style" rel="stylesheet" href="assets/css/shine.css">
+
+</head> 
+
+
+<body class="light-mode">
+	
+	<div class="container-fluid">
+			
+			<div class="main-content-wrapper">
+				<div class="container-fluid">
+					<div class="top-bar text-center position-relative">
+						<div class="top-bar-inner">
+						    <a class="btn btn-primary top-bar-cta" href="assets/images/cvresumemhudanlilmuttaqien.pdf" download>CV Resume</a>
+						</div><!--//top-bar-inner-->
+						
+					</div><!--//top-bar-->
+			
+					<div class="resume-wrapper mx-auto rounded-2">
+						<div class="resume-header px-4 px-lg-5">
+							<div class="resume-profile-holder text-center">
+								<img class="resume-profile-pic rounded-circle" src="assets/images/dansker.png" alt="image">
+								
+								<h2 class="resume-name text-uppercase">Dansker ID</h2>
+								<div class="resume-role-title text-uppercase">Desainer Website</div>
+								<div class="resume-contact mt-4">
+									<ul class="resume-contact-list list-unstyled list-inline mb-0 justify-content-between">
+										<li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-whatsapp me-2"></i>081210281781</li>
+										<li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-envelope me-2"></i>mhudanlilmuttaqien@gmail.com</li>
+										<li class="list-inline-item me-lg-5"><i class="resume-contact-icon bi bi-globe me-2"></i>www.danskersweetid.my.id</li>
+									</ul>
+								</div><!--//resume-contact-->
+							</div><!--//profile-holder-->
+						</div><!--//resume-header-->
+						
+						<div class="resume-body p-4 p-lg-5">
+							<div class="row">
+								<div class="col-main col-12 col-lg-8 pe-lg-4">
+									<section class="resume-summary-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-2 py-lg-3"><i class="resume-section-heading-icon bi bi-person me-2"></i>Tentang</h3>
+										<div class="resume-summary-desc">
+											"Halo Semuanya, saya ingin memperkenalkan diri. Nama saya M Hudan Lilmuttaqien dan Anda bisa memanggil saya Hudan/Dansker. Di bulan Maret 2024 saya memasuki semester 4 dalam kuliah, saya mengambil jurusan sistem informasi di BSI Merdeka. Saya sangat menyukai jurusan tersebut walaupun sedikit rumit atau di pahami seperti DIA. Saya selalu berdoa, agar apa yang ilmu dosen berikan kepada saya dan yang saya pelajari dapat menjadi sebuah ilmu yang bermafaat."
+										</div>
+									</section><!--//resume-section-->
+									
+									<hr>
+									
+									<section class="resume-experience-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-briefcase me-2"></i>Pengalaman Kerja</h3>
+										<div class="resume-timeline position-relative">
+										    <article class="resume-timeline-item position-relative pb-5">
+											    
+											    <div class="resume-timeline-item-header mb-2">
+												    
+												    <div class="resume-position-meta d-flex justify-content-between mb-1">
+													    <div class="resume-position-time">2024 - Present</div>
+													    <div class="resume-company-name">MTs Fathussa'adah</div>
+												    </div>
+											        <h3 class="resume-position-title mb-1">Guru</h3>
+
+											    </div><!--//resume-timeline-item-header-->
+											    <div class="resume-timeline-item-desc">
+												 
+												   
+												    <ul class="resume-timeline-list">
+													    <li>Pelajaran TIK, SENBUD, PENJAS, BTQ.</li>
+													    <li>Sekolahan yang sangat nyaman ketika saya mengajar atau memberikan sebuah ilmu kepada siswa/siswi.</li>
+													    
+													    <li>Siswa/Siswi yang sangat sopan santun dan mentaati peraturan sekolah.</li>
+													    <li>Semoga ilmu yang saya berikan dapat menjadi manfaat untuk masa depan.</li>
+												    </ul>
+												    
+											    </div><!--//resume-timeline-item-desc-->
+		
+										    </article><!--//resume-timeline-item-->
+								    
+										    <article class="resume-timeline-item position-relative pb-5">
+
+											    <div class="resume-timeline-item-header mb-2">
+												    
+												    <div class="resume-position-meta d-flex justify-content-between mb-1">
+													    <div class="resume-position-time">2021 - 2022</div>
+													    <div class="resume-company-name">PT. Sari Multi Utama</div>
+												    </div>
+											        <h3 class="resume-position-title mb-1">Operator Produksi</h3>
+
+											    </div><!--//resume-timeline-item-header-->
+											    <div class="resume-timeline-item-desc">
+												    <ul class="resume-timeline-list">
+													    
+														<li>Tempat yang sangat luas dan difasilitasi dengan kantin</li>
+													    <li>Tempat yang nyaman dan ramah semua karyawan nya.</li>
+													    <li>Pabrik tersebut memproduksikan sebuah botol.</li>
+												    </ul>
+												    
+											    </div><!--//resume-timeline-item-desc-->
+		
+										    </article><!--//resume-timeline-item-->
+										    <article class="resume-timeline-item position-relative">
+
+											    <div class="resume-timeline-item-header mb-2">
+												    
+												    <div class="resume-position-meta d-flex justify-content-between mb-1">
+													    <div class="resume-position-time">2020 - 2021</div>
+													    <div class="resume-company-name">Tobiwos Laundry</div>
+												    </div>
+											        <h3 class="resume-position-title mb-1">Cleaning</h3>
+
+											    </div><!--//resume-timeline-item-header-->
+											    <div class="resume-timeline-item-desc">
+												    <ul class="resume-timeline-list">
+													    
+													    <li>Tempat yang sangat luas dan difasilitasi dengan kantin</li>
+													    <li>Tempat yang nyaman dan ramah semua karyawan nya.</li>
+													    <li>Perusahan tersebut mengolah atau membersihkan sebuah bantal,handuk,guling,dll.</li>
+													   
+												    </ul>
+												    
+											    </div><!--//resume-timeline-item-desc-->
+		
+										    </article><!--//resume-timeline-item-->
+							            </div><!--//resume-timeline-->
+									</section><!--//resume-experience-section-->
+								</div><!--//col-8-->
+								<div class="col-12 col-lg-4 ps-lg-4">
+									
+									<section class="resume-skills-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-gear me-2"></i>Belajar</h3>
+										
+										<ul class="list-unstyled">
+								        <li class="mb-2">
+								            <div class="resume-skill-name">PHP</div>
+											<div class="progress resume-progress" role="progressbar" aria-label="Basic example" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100">
+											  <div class="progress-bar resume-progress-bar" style="width: 98%"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">CSS</div>
+								            <div class="progress resume-progress" role="progressbar" aria-label="Basic example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+											  <div class="progress-bar resume-progress-bar" style="width: 95%"></div>
+											</div>
+									        
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">HTML</div>
+									        <div class="progress resume-progress" role="progressbar" aria-label="Basic example" aria-valuenow="92" aria-valuemin="0" aria-valuemax="100">
+											  <div class="progress-bar resume-progress-bar" style="width: 92%"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name">PYTHON</div>
+									        <div class="progress resume-progress" role="progressbar" aria-label="Basic example" aria-valuenow="86" aria-valuemin="0" aria-valuemax="100">
+											  <div class="progress-bar resume-progress-bar" style="width: 86%"></div>
+											</div>
+								        </li>
+
+							        </ul>
+										
+									</section><!--//resume-section-->
+									
+									<hr>
+									
+									<section class="resume-skills-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-person-gear me-2"></i>Keterampilan</h3>
+										<ul class="list-inline">
+								            <li class="list-inline-item"><span class="badge resume-skill-badge">Desainer Web</span></li>
+								            <li class="list-inline-item"><span class="badge resume-skill-badge">Konten Kreator</span></li>
+								            
+								            <li class="list-inline-item"><span class="badge resume-skill-badge">Editting</span></li>
+								            <li class="list-inline-item"><span class="badge resume-skill-badge">Public Speaking</span></li>
+								            
+								            				           
+							            </ul>
+									</section><!--//resume-section-->
+									
+									<hr>
+									
+									<section class="resume-projects-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-code-slash me-2"></i>Desainer Web</h3>
+										<div class="item">
+											<h4 class="item-heading"><i class="item-icon bi bi-square-fill me-2"></i><a target=_blank href="https://danskersweetid.my.id/desainerweb/ppdb">PPDB SEKOLAH</a></h4>
+											<div class="item-desc">
+												Program pendaftaran calon siswa/siswi dengan berbasis online.
+											</div>
+										</div><!--//item-->
+										<div class="item">
+											<h4 class="item-heading"><i class="item-icon bi bi-square-fill me-2"></i><a target=_blank href="https://danskersweetid.my.id/desainerweb/smmppob/">SMMPPOB TOKO ONLINE</a></h4>
+											<div class="item-desc">
+												Program penjualan digital atau fisik dengan secara online.
+											</div>
+										</div><!--//item-->
+										<div class="item">
+											<h4 class="item-heading"><i class="item-icon bi bi-square-fill me-2"></i><a target=_blank href="https://danskersweetid.my.id/desainerweb/absensiscanner/">ABSENSI VIA BARCODE</a></h4>
+											<div class="item-desc">
+												Program absensi murid/karyawan dengan secara barcode (menggunakan alat barcode).
+											</div>
+										</div><!--//item-->
+										<div class="item">
+											<h4 class="item-heading"><i class="item-icon bi bi-square-fill me-2"></i><a target=_blank href="https://danskersweetid.my.id/desainerweb/absensiweb/admin">ABSENSI MANUAL WEB</a></h4>
+											<div class="item-desc">
+												Program absensi murid dengan berbasis website.
+											</div>
+										</div><!--//item-->
+										<div class="item">
+											<h4 class="item-heading"><i class="item-icon bi bi-square-fill me-2"></i><a target=_blank href="https://danskersweetid.my.id/desainerweb/resumemaker">CV RESUME MAKER</a></h4>
+											<div class="item-desc">
+												Program ini dibuat agar para pencari pekerja dapat mudah menggunakan tampilan professional.
+											</div>
+										</div><!--//item-->
+									</section>
+									
+									<hr>
+									
+									<section class="resume-educate-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-book me-2"></i>Pendidikan</h3>
+										<ul class="list-unstyled">
+										    <li class="mb-2">
+										        <div class="resume-degree font-weight-bold">Automotif</div>
+										        <div class="resume-degree-org">Smk Mekanika Cirebon</div>
+										        <div class="resume-degree-time">2017 - 2020</div>
+										    </li>
+										    <li>
+										        <div class="resume-degree font-weight-bold">Siswa</div>
+										        <div class="resume-degree-org">MTs Fathussa'adah</div>
+										        <div class="resume-degree-time">2014 - 2017</div>
+										    </li>
+											<li>
+										        <div class="resume-degree font-weight-bold">Siswa</div>
+										        <div class="resume-degree-org">MI Fathussa'adah</div>
+										        <div class="resume-degree-time">2008 - 2014</div>
+										    </li>
+									    </ul>
+									</section><!--//resume-section-->
+									
+									<hr>
+									
+									<section class="resume-lang-section resume-section">
+										<h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3"><i class="resume-section-heading-icon bi bi-translate me-2"></i>Languages</h3>
+										 <ul class="list-unstyled resume-lang-list">
+										    <li class="mb-2">
+										        <div class="resume-lang-name">Indonesia</div>
+										        <div class="resume-level-indicator row gx-0 flex-nowrap">
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-half"></span></div>
+										        </div><!--//resume-level-indicator-->
+										    </li>
+										     <li class="mb-2">
+										        <div class="resume-lang-name">Inggris</div>
+										        <div class="resume-level-indicator row gx-0 flex-nowrap">
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item item-full"></span></div>
+											        <div class="col"><span class="item"></span></div>
+										        </div><!--//resume-level-indicator-->
+										    </li>
+										</ul>
+										<ul class="social-list list-inline mx-auto">
+										    <li class="list-inline-item me-0 me-md-1 me-lg-2"><a target="_blank" href="https://www.facebook.com/danskerid"><i class="fa-brands fa-facebook fa-fw"></i></a></li>
+										    <li class="list-inline-item me-0 me-md-1 me-lg-2"><a target="_blank" href="https://www.instagram.com/dansker.id"><i class="fa-brands fa-instagram fa-fw"></i></a></li>
+										    <li class="list-inline-item me-0 me-md-1 me-lg-2"><a target="_blank" href="https://wa.me/+6281210281781"><i class="fa-brands fa-whatsapp fa-fw"></i></a></li>
+										    <li class="list-inline-item me-0 me-md-1 me-lg-2"><a target="_blank" href="https://www.youtube.com/@danskeridofficial"><i class="fa-brands fa-youtube fa-fw"></i></a></li>
+									    </ul><!--//social-list-->
+									</section>
+									
+									
+									
+								</div><!--//col-side-->
+							</div><!--//row-->
+					    </div><!--//resume-body-->
+				    </div><!--//resume-wrapper-->
+				</div><!--//container-fluid-->
+			</div><!--//main-content-wrapper-->
+		
+		
+		</div><!--//row-->
+	
+	</div><!--//container-->
+	
+	<footer class="footer text-center py-4">
+		<!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+		<small class="copyright">Designed with <span class="sr-only">love</span><i class="bi bi-heart-fill" style="color:#fe655c"></i> by <a class="theme-link" href="http://danskersweetid.my.id" target="_blank">danskerid</a> for developers</small>
+	</footer>
+
+</body>
